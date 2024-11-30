2f325f3c5a285cb9f061540e6648177b5a7383e1
jeevak25 <jeevakwankhede25@gmail.com>
2024-11-30 18:05:48 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const Home = () => {
+  return (
+      <div>
+          <Typography sx={{ margin: "5%" }} variant="h3" align="center">
+              CA II CLOUD COMPUTING
+          </Typography>
+        <Typography sx={{ margin: "5%" }} variant="h3" align="center">
+          PRAJWAL HALDE
+        </Typography>
+          <Typography sx={{ margin: "5%" }} variant="h3" align="center">
+              B414 PRN 2146491245092
+          </Typography>
+        <Typography sx={{ margin: "5%" }} variant="h3" align="center">
+          Job Listing APP
+        </Typography>
+        <div>
+          <ul className="ul">
+            <li>
+              <Button sx={{ margin: "2% 3%" }} variant="outlined">
+                <Link to="/login" className="link">
+                  Login
+                </Link>
+              </Button>
+            </li>
+            <li>
+              <Button sx={{ margin: "2% 3%" }} variant="outlined">
+                <Link to="/signup" className="link">
+                  Signup
+                </Link>
+              </Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+  );
+
+
+};
+
+console.log(`Here : ${process.env.REACT_APP_API_URL}`); // Should log http://localhost:8080 if .env is set up correctly
+export default Home;
